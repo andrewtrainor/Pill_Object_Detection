54575de9cce2319265575d70f4fc27d6519a0b1e
Prism <andrewtrainor20@gmail.com>
2020-12-26 09:20:12 -0700

    added pytorch

--- pytorch_obj_det/test.py
+++ pytorch_obj_det/test.py
@@ -4,7 +4,7 @@ import torch
 import torch.utils.data
 from PIL import Image
 import pandas as pd
-from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor, fasterrcnn_resnet50_fpn
 from engine import train_one_epoch, evaluate
 import utils
 import transforms as T
@@ -57,7 +57,7 @@ dataset = RaccoonDataset(root='../Pill_Object_Detection', data_file = "../Pill_O
 
 def get_model(num_classes):
    # load an object detection model pre-trained on COCO
-   model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+   model = fasterrcnn_resnet50_fpn(pretrained=True)
 # get the number of input features for the classifier
    in_features = model.roi_heads.box_predictor.cls_score.in_features
    # replace the pre-trained head with a new on
@@ -78,19 +78,19 @@ def get_transform(train):
 
 
 # use our dataset and defined transformations
-dataset = RaccoonDataset(root= "<your path>/raccoon_dataset",
-          data_file= "<your 
-          path>/raccoon_dataset/data/raccoon_labels.csv",
-          transforms = get_transform(train=True))
-dataset_test = RaccoonDataset(root= "<your path>/raccoon_dataset"
-               data_file= "<your
-               path>/raccoon_dataset/data/raccoon_labels.csv",
-               transforms = get_transform(train=False))
+dataset = RaccoonDataset(root='../Pill_Object_Detection', 
+                        data_file = "../Pill_Object_Detection/all_labels.csv",
+                        transforms = get_transform(train=True))
+
+dataset_test = RaccoonDataset(root='../Pill_Object_Detection', 
+                        data_file = "../Pill_Object_Detection/all_labels.csv",
+                        transforms = get_transform(train=False))
+
 # split the dataset in train and test set
 torch.manual_seed(1)
 indices = torch.randperm(len(dataset)).tolist()
-dataset = torch.utils.data.Subset(dataset, indices[:-40])
-dataset_test = torch.utils.data.Subset(dataset_test, indices[-40:])
+dataset = torch.utils.data.Subset(dataset, indices[:-8])
+dataset_test = torch.utils.data.Subset(dataset_test, indices[-8:])
 # define training and validation data loaders
 data_loader = torch.utils.data.DataLoader(
               dataset, batch_size=2, shuffle=True, num_workers=4,
@@ -99,3 +99,38 @@ data_loader_test = torch.utils.data.DataLoader(
          dataset_test, batch_size=1, shuffle=False, num_workers=4,
          collate_fn=utils.collate_fn)
 print("We have: {} examples, {} are training and {} testing".format(len(indices), len(dataset), len(dataset_test)))
+
+# To check if GPU is available
+torch.cuda.is_available()
+#"""
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+# our dataset has two classes only - raccoon and not racoon
+num_classes = 2
+# get the model using our helper function
+model = get_model(num_classes)
+# move model to the right device
+model.to(device)
+# construct an optimizer
+params = [p for p in model.parameters() if p.requires_grad]
+optimizer = torch.optim.SGD(params, lr=0.005,
+                            momentum=0.9, weight_decay=0.0005)
+# and a learning rate scheduler which decreases the learning rate by # 10x every 3 epochs
+lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer,
+                                               step_size=3,
+                                               gamma=0.1)
+
+# let's train it for 10 epochs
+num_epochs = 10
+for epoch in range(num_epochs):
+   # train for one epoch, printing every 10 iterations
+   train_one_epoch(model, optimizer, data_loader, device, epoch,
+                   print_freq=10)
+# update the learning rate
+   lr_scheduler.step()
+   # evaluate on the test dataset
+   evaluate(model, data_loader_test, device=device)
+
+# To save trained model
+os.mkdir("~/repos/pytorch_obj_det/Pillcount/")
+torch.save(model.state_dict(), "~/repos/pytorch_obj_det/Pillcount/model")
+#"""
